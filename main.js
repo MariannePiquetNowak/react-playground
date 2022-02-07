fe20b9963ce7fbe27a95f404ff8cb45aa53ce1b4
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-07 17:00:49 +0100

    class Component exercice

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
+
 const firstName = 'r0ulito';
 const lastName = 'formateur';
 
 function FirstName(props) {
 
-    /*
-    // Solution avec bonus
-    const formatFirstName = (firstName) => {
-        return firstName[0].toUpperCase() + firstName.substr(1);
-    }
-
-    return <span>{formatFirstName(props.text)}</span>
-    */
-
-    // Solution sans bonus
     return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
 }
 
 function LastName(props) {
 
-    /*
-    // Solution avec bonus
-    const formatLastName = (lastName) => {
-        return lastName.toUpperCase();
-    }
-
-    return <span>{formatLastName(props.text)}</span>
-    */
-
-    // Solution sans bonus
     return <span className="red-text">{props.text.toUpperCase()}</span>
 
 }
 
-const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
+// const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+/**
+ * CLASS COMPONENT
+ */
+class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: "Marianne",
+            lastName: "Piquet"
+        }
+    }
+    render() {
+
+        return(
+
+            <h1>Hello{" "}
+                <FirstName text={this.state.firstName.concat(" ")}/>
+                <LastName text={this.state.lastName} />
+            </h1>
+            
+        )
+    }
+}
+
+/**
+ * FUNCTIONNAL COMPONENT
+ */
+
+ReactDOM.render(<HelloWorld />, document.querySelector('#app'));
