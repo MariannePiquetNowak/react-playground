8404b35b3d5c54e59f49bfa5d676a1ff81233d78
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-07 14:09:26 +0100

    function format

--- main.js
+++ main.js
@@ -1,32 +1,56 @@
-var firstName = "Emmanuel";
-var lastName = "Macron";
-
-// const helloWorld = React.createElement("h1", {}, "Hello world!");
 function strUcFirst(a) {
-    return (a + '').charAt(0).toUpperCase() + a.substr(1);
+  return (a + '').charAt(0).toUpperCase() + a.substr(1);
 }
 
-var hello = React.createElement(
+function formatComponent(user) {
+  return React.createElement(
     "div",
     null,
     React.createElement(
-        "h1",
-        null,
-        "Hello"
+      "h1",
+      null,
+      user.text
     ),
     React.createElement(
-        "span",
-        { className: "firstname" },
-        strUcFirst(firstName),
-        " "
+      "span",
+      { className: "firstname" },
+      strUcFirst(user.firstName),
+      " "
     ),
     React.createElement(
-        "span",
-        { className: "lastname" },
-        lastName.toUpperCase()
+      "span",
+      { className: "lastname" },
+      user.lastName.toUpperCase()
     )
+  );
+}
+
+var user = {
+  firstName: "Emmanuel",
+  lastName: "Macron",
+  text: "Nous sommes en guerre"
+};
+
+var component = React.createElement(
+  "section",
+  null,
+  formatComponent(user)
 );
 
-console.log(hello);
+// const firstName = "Emmanuel";
+// const lastName = "Macron";
 
-ReactDOM.render(hello, document.querySelector("#app"));
+// // const helloWorld = React.createElement("h1", {}, "Hello world!");
+// function strUcFirst(a){return (a+'').charAt(0).toUpperCase()+a.substr(1);}
+
+// const hello = (
+//     <div>
+//     <h1>Hello</h1>
+//     <span className="firstname">{strUcFirst(firstName)} </span>
+//     <span className="lastname">{lastName.toUpperCase()}</span>
+//   </div>
+// )
+
+console.log(component);
+
+ReactDOM.render(component, document.querySelector("#app"));
--- src/main.js
+++ src/main.js
@@ -1,18 +1,43 @@
-const firstName = "Emmanuel";
-const lastName = "Macron";
-
-// const helloWorld = React.createElement("h1", {}, "Hello world!");
 function strUcFirst(a){return (a+'').charAt(0).toUpperCase()+a.substr(1);}
 
-const hello = (
+function formatComponent(user) {
+  return (
     <div>
-    <h1>Hello</h1>
-    <span className="firstname">{strUcFirst(firstName)} </span>
-    <span className="lastname">{lastName.toUpperCase()}</span>
-  </div>
-)
+      <h1>{user.text}</h1>
+      <span className="firstname">{strUcFirst(user.firstName)} </span>
+      <span className="lastname">{user.lastName.toUpperCase()}</span>
+    </div>
+  )
+}
 
-console.log(hello)
+const user = {
+  firstName: "Emmanuel",
+  lastName: "Macron",
+  text: "Nous sommes en guerre"
+}
 
 
-ReactDOM.render(hello, document.querySelector("#app"));
+const component = (
+  <section>
+    {formatComponent(user)}
+  </section>
+);
+
+
+// const firstName = "Emmanuel";
+// const lastName = "Macron";
+
+// // const helloWorld = React.createElement("h1", {}, "Hello world!");
+// function strUcFirst(a){return (a+'').charAt(0).toUpperCase()+a.substr(1);}
+
+// const hello = (
+//     <div>
+//     <h1>Hello</h1>
+//     <span className="firstname">{strUcFirst(firstName)} </span>
+//     <span className="lastname">{lastName.toUpperCase()}</span>
+//   </div>
+// )
+
+console.log(component)
+
+ReactDOM.render(component, document.querySelector("#app"));
