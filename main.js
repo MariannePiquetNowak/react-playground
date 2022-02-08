3ad47f4a9f8681724e1e5712e10249bb8a8f137e
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-08 16:28:22 +0100

    correction

--- main.js
+++ main.js
@@ -81,3 +81,47 @@ function UserGreeting(props) {
   }
 
 ReactDOM.render(<Greeting isLoggedIn={false} />, document.querySelector("#app"));
+
+/**
+ * CORRECTION EXO 7 
+ */
+
+ function UserGreeting(props) {
+  return <h1>Bienvenue !</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Veuillez vous connecter</h1>;
+}
+function Greeting(props) {
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+    const handleLogIn = (e) => {
+        e.preventDefault();
+        setIsLoggedIn(true);
+    }
+
+    const handleLogOut = (e) => {
+        e.preventDefault();
+        setIsLoggedIn(false);
+    }
+
+  return(
+      <React.Fragment>
+          {isLoggedIn ? 
+          <React.Fragment>
+              <UserGreeting />
+              <button onClick={handleLogOut}>Se déconnecter</button>
+          </React.Fragment> : 
+          <React.Fragment>
+              <GuestGreeting />
+              <button onClick={handleLogIn}>Se connecter</button>
+          </React.Fragment> }
+      </React.Fragment>
+  )
+}
+
+ReactDOM.render(
+  <Greeting />,
+  document.querySelector('#app')
+);
