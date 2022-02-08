ac4f9acd5d5c942f58c61496e6c97b5b6dceeecb
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-08 13:54:10 +0100

    ...

--- main.js
+++ main.js
@@ -67,3 +67,81 @@ const HelloWorldTwo = () => {
 }
 
 ReactDOM.render(<HelloWorldTwo />, document.querySelector('#app'));
+
+/**
+ * CORRECTION 
+ */
+
+/* 
+// Étape 1
+class Clock extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Bonjour, monde !</h1>
+          <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+
+ReactDOM.render(<Clock date={new Date()}/>, document.querySelector('#app')); 
+
+*/
+
+/* 
+
+// Étape 2
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date()};
+    }
+  render() {
+    return (
+      <div>
+        <h1>Bonjour, monde !</h1>
+        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.querySelector('#app')); 
+
+
+ */
+
+// Étape 3
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date()};
+    }
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(),1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+        );
+    }
+}
+
+ReactDOM.render(<Clock />, document.querySelector('#app')); 
