be4ef4dd760b748f5812c9571f1fc27f8f52c4dc
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-09 09:40:13 +0100

    create visit card

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-function UserGreeting(props) {
-    return <h1>Bienvenue !</h1>;
-  }
-  
-  function GuestGreeting(props) {
-    return <h1>Veuillez vous connecter</h1>;
-  }
-  function Greeting(props) {
-      const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+const VisitCard = () => {
 
-      const handleLogIn = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(true);
-      }
+const [users, setUser] = React.useState([]);
+const url = "https://jsonplaceholder.typicode.com/users";
 
-      const handleLogOut = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(false);
-      }
+const getUser = async () => {
+  try {
+    const results = await axios(url);
+    const datas = results.data;
+    setUser(datas);
+  } catch (err) {console.error(err)}
+}
 
-    return(
-        <React.Fragment>
-            {isLoggedIn ? 
-            <React.Fragment>
-                <UserGreeting />
-                <button onClick={handleLogOut}>Se déconnecter</button>
-            </React.Fragment> : 
-            <React.Fragment>
-                <GuestGreeting />
-                <button onClick={handleLogIn}>Se connecter</button>
-            </React.Fragment> }
-        </React.Fragment>
-    )
-  }
-  
-  ReactDOM.render(
-    <Greeting />,
-    document.querySelector('#app')
-  );
+
+React.useEffect(() => {
+  getUser();
+  console.log(users) // => Question à Julien : pourquoi ça renvoi un tableau vide ?
+},[])
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li className="container" key={user.id}>
+            <h1>{user.name}</h1>
+            <h4>{user.company.name}</h4>
+        <div style={{width: "100%", display: "flex", justifyContent: "center" }}>
+        <div className="info">
+            <p><span>email:</span> {user.email}</p>
+            <p><span>Phone Number:</span> {user.phone}</p>
+            <p><span>Website :</span> {user.website}</p>
+          </div>
+        </div>
+          
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+ReactDOM.render(<VisitCard />, document.querySelector("#app"));
