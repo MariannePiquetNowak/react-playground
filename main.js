3b9e901fb3f5b902fdf3fa04e30dad073934e9b9
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-09 14:47:12 +0100

    commencement méthode 2

--- main.js
+++ main.js
@@ -1,4 +1,89 @@
 const App = () => {
+	const [value, setValue] = React.useState({
+		name: "test",
+		textarea: "nfdlshfdkhsfklds",
+		fruit: "coconut"
+	});
+  
+	const handleSubmit = (e) => {
+		//   alert(`Le nom a été soumis : ${name} // ${textarea} // ${select}` );
+		console.log("alert")
+	  	e.preventDefault();
+	};
+  
+	const handleChange = (e) => {
+		const target = e.target;
+		if(target.type === "text") {
+			setValue(target.value);
+		}
+		if(target.type === "textarea") {
+			setValue(target.value);
+		}
+		if(target.name === "select") {
+			setValue(target.value)
+		}
+	};
+  
+	return (
+	  <form 
+		  onSubmit={handleSubmit} 
+		  style={{
+			  display: "flex",
+			  flexDirection: "column",
+			  width: "50%"
+		  }}>
+		<NameForm name="input" change={handleChange} value={value.name}/>
+		<EssayForm name="textarea" change={handleChange} value={value.textarea}/>
+  
+		<FlavorForm name="textarea" change={handleChange} value={value.fruit}/>
+  
+		<input type="submit" value="Envoyer" style={{width: "200px", marginTop: "1em"}}/>
+	  </form>
+	);
+  };
+  
+  const NameForm = (props) => {
+	return (
+	  <React.Fragment>
+		<label style={{display: "flex", flexDirection: "column", marginTop: "1em"}}>
+		  Nom :
+		  <input type="text" value={props.value} onChange={props.change} />
+		</label>
+	  </React.Fragment>
+	);
+  };
+  
+  const EssayForm = (props) => {
+	
+	return (
+	  <React.Fragment>
+		<label style={{display: "flex", flexDirection: "column", marginTop: "1em"}}>
+		  Essay:
+		  <textarea value={props.value} onChange={props.change} />
+		</label>
+	  </React.Fragment>
+	);
+  };
+  
+  const FlavorForm = (props) => {
+  
+	return (
+	  <React.Fragment>
+		<label>
+		  Choisissez votre parfum favori :
+		  <select name="select" value={props.value} onChange={props.change}>
+			<option value="grapefruit">Pamplemousse</option>
+			<option value="lime">Citron vert</option>
+			<option value="coconut">Noix de coco</option>
+			<option value="mango">Mangue</option>
+		  </select>
+		</label>
+	  </React.Fragment>
+	);
+  };
+
+/*
+const App = () => {
   const [name, setName] = React.useState('');
   const [textarea, setTextArea] = React.useState('Écrivez un essai à propos de votre élément du DOM préféré');
   const [select, setSelect] = React.useState('coconut')
@@ -15,6 +100,7 @@ const App = () => {
 	} 
 	if(targetType === "textarea") {
 		setTextArea(e.target.value)
+		
 	}
 	if(e.target.name === "select") {
 		setSelect(e.target.value)
@@ -79,6 +165,6 @@ const FlavorForm = (props) => {
   );
 };
 
-
+*/
 
 ReactDOM.render(<App />, document.querySelector("#app"));
