271e460c13abd70d974d5053f4b4abb2eb9df31d
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-09 10:20:28 +0100

    exo9 : part 1 ok

--- main.js
+++ main.js
@@ -1,37 +1,82 @@
-function App(props) {
-    React.useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(usersObject => setUsers(usersObject))
-    }, [])
+const App = () => {
+  return <React.Fragment>coucou</React.Fragment>;
+};
 
-    const [users, setUsers] = React.useState([]);
+const NameForm = (props) => {
+  const [value, setValue] = React.useState("");
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  };
 
+  const handleSubmit = (e) => {
+    alert("Le nom a été soumis : " + value);
+    e.preventDefault();
+  };
 
-    return(
-        <React.Fragment>
-        {
-            users.map((u) =>(
-                <UserCard key={u.id} user={u}/>
-            ))
-        }
-        </React.Fragment>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nom :
+        <input type="text" value={value} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
+};
 
-function UserCard({user}) {
-    
+const EssayForm = (props) => {
+  const [value, setValue] = React.useState("Écrivez un essai à propos de votre élément du DOM préféré");
 
-    return (
-        <ul className="user-card">
-            <li>{user.name}</li>
-            <li>{user.email}</li>
-            <li>{user.company.name}</li>
-            <li>{user.phone}</li>
-            <li>{user.website}</li>
-        </ul>
-    )
-}
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    console.log("Test", e.target.value);
+  };
 
-ReactDOM.render(<App/>, document.querySelector('#app'))
+  const handleSubmit = (e) => {
+    alert("Un essai a été envoyé : " + value);
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Essay:
+        <textarea value={value} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
+};
+
+const FlavorForm = (props) => {
+
+  const [value, setValue] = React.useState('coconut');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    console.log(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    alert('Votre parfum favori est : ' + value);
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Choisissez votre parfum favori :
+        <select value={value} onChange={handleChange}>
+          <option value="grapefruit">Pamplemousse</option>
+          <option value="lime">Citron vert</option>
+          <option value="coconut">Noix de coco</option>
+          <option value="mango">Mangue</option>
+        </select>
+      </label>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
+};
+
+ReactDOM.render(<FlavorForm />, document.querySelector("#app"));
