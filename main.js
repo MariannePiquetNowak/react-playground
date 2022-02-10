1c744ab95c7eb8cba44c64ad0e5f2bce969985b7
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-10 11:12:11 +0100

    add correction

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const PokemonCard = (props) => {
 			setPokemon(data);
 			setType(data.types[0].type.name);
 			setAbilities(data.abilities);
-			// const artObj = Object.values(data.sprites.other)
+			// gitconst artObj = Object.values(data.sprites.other)
 			// setArtwork(artObj[2].front_default);
 			setArtwork(data.sprites.other["official-artwork"].front_default)
 
@@ -103,3 +103,132 @@ const PokemonCard = (props) => {
 }
 
 ReactDOM.render(<App />, document.querySelector("#app"));
+
+/**
+ * CORRECTION
+ */
+
+/*
+
+const colours = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
+
+const App = () => {
+  const number = 25;
+  const [pokemons, setPokemon] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [previousUrl, setPreviousUrl] = React.useState("");
+  const [nextUrl, setNextUrl] = React.useState("");
+
+  React.useEffect(() => {
+    fetchPokemons(`https://pokeapi.co/api/v2/pokemon?limit=15&offset=0`);
+  }, []);
+
+  const fetchPokemons = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data.results);
+        setPreviousUrl(data.previous);
+        setNextUrl(data.next);
+      });
+  };
+
+  React.useEffect(() => {
+    if (pokemons.length === 0) setIsLoading(true);
+    if (pokemons.length > 0) setIsLoading(false);
+  }, [pokemons]);
+
+  const changeOffset = (value) => {
+    if (value === "<") {
+      if (previousUrl === null) return;
+      fetchPokemons(previousUrl);
+    } else {
+      fetchPokemons(nextUrl);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Pagination onButtonClick={changeOffset} />
+      {isLoading && <h1>Loading...</h1>}
+      {pokemons.map((pokemon) => (
+        <PokemonCard key={pokemon.name} {...pokemon} />
+      ))}
+      <Pagination onButtonClick={changeOffset} />
+    </React.Fragment>
+  );
+};
+
+const PokemonCard = (props) => {
+  const [pokemon, setPokemon] = React.useState({});
+  const [type, setType] = React.useState("");
+  const [style, setStyle] = React.useState({});
+  const [sprite, setSprite] = React.useState("");
+  React.useEffect(() => {
+    fetch(props.url)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+        setType(data.types[0].type.name);
+        setSprite(data.sprites.other["official-artwork"].front_default);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    setStyle({
+      backgroundColor: colours[type],
+      boxShadow: `${colours[type]} 1px 2px 3px`,
+    });
+  }, [type]);
+
+  return (
+    <div className="pokemon-card" style={style}>
+      <img className="pokemon-img" src={sprite} alt="" />
+      <div>N° {String(pokemon.id).padStart(3, 0)}</div>
+      <div>Nom: {pokemon.name}</div>
+      <div>Poids: {pokemon.weight / 10} kgs</div>
+    </div>
+  );
+};
+
+const Pagination = ({ onButtonClick }) => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <button
+        onClick={({ target: { value } }) => onButtonClick(value)}
+        value="<"
+      >
+        {"<"}
+      </button>
+      <button
+        onClick={({ target: { value } }) => onButtonClick(value)}
+        value=">"
+      >
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"));
+
+*/
