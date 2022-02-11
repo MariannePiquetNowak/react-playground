b723456e95adc35c355aa2637735c77fd0ca6152
MariannePiquetNowak <mariannexpiquet@gmail.com>
2022-02-11 09:26:58 +0100

    Part 1 - create  event keyPress

--- comme-vous-le-souhaitez/src/App.js
+++ comme-vous-le-souhaitez/src/App.js
@@ -1,7 +1,22 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import { useState } from 'react'
 
 function App() {
+
+  const [newTodo, setNewTodo] = useState({
+        id: 1,
+        title: 'ma super tâche',
+        isCompleted : false,
+        isEditing: false
+  });
+
+  const handleKeyEnter = (e) => {
+    if(e.key === "Enter") {
+      console.log('validate')
+    }
+  }
+
   return (
     <div className="App">
       {/* Le composant doit afficher :
@@ -9,8 +24,15 @@ function App() {
         - le composant qui affiche les filtres
         - La liste des todos 
       */}
+      <Input handleKeyEnter={handleKeyEnter} />
     </div>
   );
+}
+
+const Input = ({handleKeyEnter, ...props}) => {
+  return (
+    <input onKeyPress={handleKeyEnter}/>
+  )
 }
 
 export default App;
